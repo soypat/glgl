3f4dd9f3e96bff67f6fdbcf56a35f34154ab97b0
soypat <graded.sp@gmail.com>
2024-07-22 20:36:07 -0300

    keep adding sdf example

--- examples/sdf/sdf.go
+++ examples/sdf/sdf.go
@@ -6,11 +6,11 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
 	"math"
 	"runtime"
 	"strconv"
-	"strings"
 )
 
 const addThis = 20
@@ -38,20 +38,25 @@ type Sphere struct {
 	R float32
 }
 
+const fltPrec = 8
+
 type SDFShader struct {
-	Name string
-	Body string
+	Name []byte
+	Body []byte
 }
 
-func (s *Sphere) Shader() SDFShader {
-	r := strconv.FormatFloat(float64(s.R), 'f', 16, 32)
-	if !strings.Contains(r, ".") {
-		r += "."
+func (s *Sphere) AppendShader(glsl *SDFShader) error {
+	r := float64(s.R)
+	glsl.Name = append(glsl.Name, "sphere_"...)
+	glsl.Name = strconv.AppendFloat(glsl.Name, r, 'f', fltPrec, 32)
+	if idx := bytes.IndexByte(glsl.Name, '.'); idx >= 0 {
+		// Identifiers cannot have period in name.
+		glsl.Name[idx] = 'p'
 	}
-	return SDFShader{
-		Name: "sphere_" + r,
-		Body: "float r=" + r + ";return length(p)-r;",
-	}
+	glsl.Body = append(glsl.Body, "return length(p)-"...)
+	glsl.Body = strconv.AppendFloat(glsl.Body, r, 'f', fltPrec, 32)
+	glsl.Body = append(glsl.Body, ';')
+	return nil
 }
 
 func (s *Sphere) Evaluate(positions []Vec, distances []float32) (int, error) {
@@ -69,7 +74,43 @@ func (s *Sphere) Bounds() (min, max Vec) {
 	return min, max
 }
 
-func NewSphere(radius float32) (SDF, error) {
+type SDFShaderer interface {
+	AppendShader(glsl *SDFShader) error
+}
+
+type BinaryOpShader struct {
+	s1, s2  SDFShaderer
+	opname  string
+	bodyFmt string
+}
+
+type UnionShader struct {
+	s1, s2 SDFShaderer
+}
+
+func (s *UnionShader) AppendShader(glsl *SDFShader) error {
+	body := glsl.Body
+	glsl.Name = append(glsl.Name, "union_"...)
+	id1Start := len(glsl.Name)
+	err := s.s1.AppendShader(glsl)
+	if err != nil {
+		return err
+	}
+	id2Start := len(glsl.Name)
+	err = s.s2.AppendShader(glsl)
+	if err != nil {
+		return err
+	}
+	glsl.Body = glsl.Body[:len(body)] // Remove union element bodies but retain longer
+	glsl.Body = append(glsl.Body, "return min("...)
+	glsl.Body = append(glsl.Body, glsl.Name[id1Start:id2Start]...)
+	glsl.Body = append(glsl.Body, "(p),"...)
+	glsl.Body = append(glsl.Body, glsl.Name[id2Start:]...)
+	glsl.Body = append(glsl.Body, "(p));"...)
+	return nil
+}
+
+func NewSphere(radius float32) (SDFShaderer, error) {
 	return &Sphere{R: radius}, nil
 }
 
